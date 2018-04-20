f3857057b42daf4e738d8ea89a17aec117e85d92
npc516 <35354566+npc516@users.noreply.github.com>
2018-04-20 09:24:19 -0400

    Add files via upload

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,8 +3,10 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Index from '@/components/Index'
 import Auth from '@/components/Auth'
-import Buyitnow from '@/components/Buyitnow'
-
+import Match from '@/components/Match'
+import Trainer from '@/components/Trainer'
+import Insurance from '@/components/Insurance'
+import Signupsuc from '@/components/Signupsuc'
 Vue.use(Router)
 
 export default new Router({
@@ -31,9 +33,24 @@ export default new Router({
       component: Auth
     },
     {
-      path: '/buyitnow',
-      name: 'Buyitnow',
-      component: Buyitnow
+      path: '/match',
+      name: 'Match',
+      component: Match
+    },
+    {
+      path: '/trainer',
+      name: 'Trainer',
+      component: Trainer
+    },
+    {
+      path: '/insurance',
+      name: 'Insurance',
+      component: Insurance
+    },
+    {
+      path: '/signupsuc',
+      name: 'Signupsuc',
+      component: Signupsuc
     }
   ]
 })
