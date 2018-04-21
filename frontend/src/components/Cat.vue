fd05a4f07f151271a7f59cccc6813cabb8e582a9
npc516 <35354566+npc516@users.noreply.github.com>
2018-04-21 16:00:00 -0400

    Add files via upload

---
+++ frontend/src/components/Cat.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class='formta'>
+
+<div id="trainer" v-if='!s'>
+  <div class='trainerform'>
+    <div style="display:inline; width:40; float:left; margin-right: 50px">
+     <img style='width:350px; height:300px; ' src='../assets/image/british-shorthair-cats.jpg'>
+    </div>
+    <div style=" text-align:left; display:inline; width:60px float:left">
+      <p style="color:white; text-align:left; font-size:30px">Cat Name: Bob</p>
+      <p style="color:white; text-align:left; font-size:30px">age: 3</p>
+        <p style="color:white; text-align:left; font-size:30px">Breed: British Shorthair</p>
+        <p style="color:white; text-align:left; font-size:30px">Weight: 15 pounds</p>
+        <p style="color:white; text-align:left; font-size:30px">color: grey</p>
+
+    </div>
+    <br><br>
+    
+ </div>
+<br><br><br>
+</div>
+
+</div>
+</template>
+
+<style>
+  @import '../../static/css/match.css';
+</style>
