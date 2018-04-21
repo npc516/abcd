54da323cf8916f689218d50a7516e0ad5612b804
tuboshu1222 <swxie@live.cn>
2018-04-21 16:12:07 -0400

    Add files via upload

--- frontend/src/components/AddCat.vue
+++ frontend/src/components/AddCat.vue
@@ -1,8 +1,59 @@
 <template>
-  <div class="AddCat">
-    <input type="file" @change="onFileSelected">
-    <button @click="onUpload">Upload</button>
+  <div class='form'>
+    <div id="signup" v-if='s'>
+      <h1> {{ smsg }} </h1>
+
+      <div class="field-wrap">
+        <div class="field-wrap">
+          <label v-bind:class='{active: cat_name!=="" && cat_name!==null, highlight: cat_name!=="" && cat_name!==null}'>
+            Cat Name<span class="req">*</span>
+          </label>
+          <input type="text" v-model='cat_name' required autocomplete="off" />
+        </div>
+      </div>
+
+      <div class="field-wrap">
+        <label v-bind:class='{active: cat_color!=="" && cat_color!==null, highlight: cat_color!=="" && cat_color!==null}'>
+          Cat Color<span class="req">*</span>
+        </label>
+        <input type="cat_color" v-model='cat_color' required autocomplete="off"/>
+      </div>
+
+      <div class="field-wrap">
+        <label v-bind:class='{active: breed!=="" && breed!==null, highlight: breed!=="" && breed!==null}'>
+          Breed<span class="req">*</span>
+        </label>
+        <input type="breed" v-model='breed' required autocomplete="off"/>
+      </div>
+
+      <div class="field-wrap">
+        <label v-bind:class='{active: age!=="" && age!==null, highlight: age!=="" && age!==null}'>
+          Age<span class="req">*</span>
+        </label>
+        <input type="tel" v-model='age' required autocomplete="off"/>
+      </div>
+
+      <div class="field-wrap">
+        <label v-bind:class='{active: weight!=="" && weight!==null, highlight: weight!=="" && weight!==null}'>
+          Weight<span class="req">*</span>
+        </label>
+        <input type="text" v-model='weight' required autocomplete="off"/>
+      </div>
+      <div class="field-wrap">
+        <input type="file" @change="onFileSelected">
+        <button @click="onUpload">Upload</button>
+      </div>
+      <button class="button button-block" v-on:click='sign_up()'>Upload</button>
+
+    </div>
+    <div id="login" v-if='!s'>
+      <h1> {{ lmsg }} </h1>
+
+      <button class="button button-block" v-on:click='log_in()'>Log In</button>
+    </div>
+    <img style="position:absolute; top:150px; left:980px; width:800px" src="../assets/image/icon.png">
   </div>
+
 </template>
 
 <script>
@@ -12,7 +63,14 @@ export default {
   name: 'AddCat',
   data () {
     return {
-      selectedFile: null
+      cat_name: null,
+      cat_color: null,
+      breed: null,
+      age: null,
+      weight: null,
+      s: true,
+      selectedFile: null,
+      smsg: 'Please enter information of your cat'
     }
   },
   methods: {
@@ -31,20 +89,5 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
 </style>
