538e418abdc155ee4f385cf677863763d6f0b09d
tuboshu1222 <swxie@live.cn>
2018-04-25 19:27:45 -0400

    add cat.js

---
+++ frontend/src/scripts/cat.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+var API = 'http://localhost:5000/api'
+
+export default {
+  upload (cred, cb) {
+    var data = {status: false}
+
+    axios.post(API + '/users', cred).then((res) => {
+      if (res.status === 200) {
+        localStorage.setItem('token', 'uploaded')
+        data.status = true
+      }
+      cb(null, data)
+    }).catch((err) => {
+      cb(err, data)
+    })
+  }
+}
