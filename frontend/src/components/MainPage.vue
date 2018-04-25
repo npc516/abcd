d18b7d818343ab3a470a1b774f56ba85f0017073
tuboshu1222 <swxie@live.cn>
2018-04-25 19:22:08 -0400

    change image name and add feeling kitty button

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-      <img style="position:absolute; top:120px; left:500px; width:850px" src="../assets/image/WhiteBackgroundCat.jpg">
+      <img id= "CatBg" style="position:absolute; top:120px; left:500px; width:850px" :src='imageLoad()'>
+      <button style="position:absolute; top:120px; left:1300px; width:100px" v-on:click='imageChange()'>Feeling Kitty</button>
+      <p>Show me another cat!</p>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      ipath: '1'
+    }
+  },
+  methods: {
+    imageChange () {
+      this.ipath = Math.floor(Math.random() * (4 - 1 + 1)) + 1
+    },
+    imageLoad () {
+      return require('../assets/image/' + this.ipath + '.jpg')
+    }
+  }
+}
+</script>
