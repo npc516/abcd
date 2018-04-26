310e72318b15ceb8d148fe018394203ee307245d
tuboshu1222 <swxie@live.cn>
2018-04-26 00:24:07 -0400

    cat search changed required

--- frontend/src/components/CatSearch.vue
+++ frontend/src/components/CatSearch.vue
@@ -6,64 +6,64 @@
       <div class="top-row">
         <div class="field-wrap">
           <label v-bind:class='{active: cat_name!=="" && cat_name!==null, highlight: cat_name!=="" && cat_name!==null}'>
-            Cat Name<span class="req">*</span>
+            Cat Name<span class="req"></span>
           </label>
-          <input type="text" v-model='cat_name' required autocomplete="off" />
+          <input type="text" v-model='cat_name' autocomplete="off" />
         </div>
 
         <div class="field-wrap">
           <label v-bind:class='{active: cat_id!=="" && cat_id!==null, highlight: cat_id!=="" && cat_id!==null}'>
-            Cat Id<span class="req">*</span>
+            Cat Id<span class="req"></span>
           </label>
-          <input type="text" v-model='cat_id' required autocomplete="off"/>
+          <input type="text" v-model='cat_id' autocomplete="off"/>
         </div>
       </div>
 
       <div class="field-wrap">
         <label v-bind:class='{active: breed!=="" && breed!==null, highlight: breed!=="" && breed!==null}'>
-          Breed<span class="req">*</span>
+          Breed<span class="req"></span>
         </label>
-        <input type="text" v-model='breed' required autocomplete="off"/>
+        <input type="text" v-model='breed' autocomplete="off"/>
       </div>
 
       <div class="field-wrap">
         <label v-bind:class='{active: color!=="" && color!==null, highlight: color!=="" && color!==null}'>
-          Color<span class="req">*</span>
+          Color<span class="req"></span>
         </label>
-        <input type="text" v-model='color' required autocomplete="off"/>
+        <input type="text" v-model='color' autocomplete="off"/>
       </div>
 
       <div class="field-wrap">
         <label v-bind:class='{active: weight!=="" && weight!==null, highlight: weight!=="" && weight!==null}'>
-          Weight<span class="req">*</span>
+          Weight<span class="req"></span>
         </label>
-        <input type="text" v-model='weight' required autocomplete="off"/>
+        <input type="text" v-model='weight' autocomplete="off"/>
       </div>
 
       <div class="field-wrap">
         <label v-bind:class='{active: age!=="" && age!==null, highlight: age!=="" && age!==null}'>
-          Age<span class="req">*</span>
+          Age<span class="req"></span>
         </label>
-        <input type="text" v-model='age' required autocomplete="off"/>
+        <input type="text" v-model='age' autocomplete="off"/>
       </div>
 
       <div class="field-wrap">
         <label v-bind:class='{active: eye_color!=="" && eye_color!==null, highlight: eye_color!=="" && eye_color!==null}'>
-          Eye Color<span class="req">*</span>
+          Eye Color<span class="req"></span>
         </label>
-        <input type="text" v-model='eye_color' required autocomplete="off"/>
+        <input type="text" v-model='eye_color' autocomplete="off"/>
       </div>
       <div class="field-wrap">
         <label v-bind:class='{active: hometown!=="" && hometown!==null, highlight: hometown!=="" && hometown!==null}'>
-          Hometown<span class="req">*</span>
+          Hometown<span class="req"></span>
         </label>
-        <input type="text" v-model='hometown' required autocomplete="off"/>
+        <input type="text" v-model='hometown' autocomplete="off"/>
       </div>
       <div class="field-wrap">
         <label v-bind:class='{active: sex!=="" && sex!==null, highlight: sex!=="" && sex!==null}'>
-          Sex<span class="req">*</span>
+          Sex<span class="req"></span>
         </label>
-        <input type="text" v-model='sex' required autocomplete="off"/>
+        <input type="text" v-model='sex' autocomplete="off"/>
       </div>
 
       <button class="button button-block" v-on:click='sign_up()'>Search</button>
@@ -89,7 +89,7 @@ export default {
       hometown: null,
       sex: null,
       s: true,
-      smsg: 'Please enter Searching information'
+      smsg: 'Please enter Searching criteria'
     }
   },
   methods: {
