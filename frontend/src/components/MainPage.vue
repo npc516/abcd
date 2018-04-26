07613a0c912ea21d2e98b0fe6b615d8cdf631257
tuboshu1222 <swxie@live.cn>
2018-04-25 23:04:39 -0400

    kitty button

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
       <img id= "CatBg" style="position:absolute; top:120px; left:500px; width:850px" :src='imageLoad()'>
-      <button class="button button-block" style="position:absolute; top:620px; left:500px; width:400px" v-on:click='imageChange()'>Feeling Kitty</button>
+      <button class="button button-block" style="position:absolute; top:690px; left:500px; width:400px" v-on:click='imageChange()'>Feeling Kitty</button>
       <p style="position:absolute; top:180px; left:50px; width:400px; font-size: 150%">Welcome to Ultimate Cat-Fighting Championship</p>
       <p style="position:absolute; top:400px; left:50px; width:400px; font-size: 150%">Enjoy your Cat Adventure</p>
   </div>
