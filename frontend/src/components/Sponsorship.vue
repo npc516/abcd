dd8404f95ee71039c74a0b234e91ba9f5a5005a0
ppht <edgw564731@yahoo.com>
2018-04-25 23:37:55 -0400

    remove question marks

--- frontend/src/components/Sponsorship.vue
+++ frontend/src/components/Sponsorship.vue
@@ -1,4 +1,4 @@
-<template>
+f<template>
   <div class='formta'>
     <h1 style="font-size:500px color:white font-family:arial">Check our Sponsors!</h1>
 <div id="trainer" v-if='!s'>
@@ -11,7 +11,7 @@
       <p style="color:white; text-align:left; font-size:30px">Duration: April 1st to May 1st</p>
         <p style="color:white; text-align:left; font-size:30px">payment: $998</p>
         <p style="color:white; text-align:left; font-size:30px">Contact Information: (817) 659-9378</p>
-          <p style="color:white; text-align:left; font-size:30px">Description: Chicago start up company that manages cats<br>and ??? dfas<br>?????</p>
+          <p style="color:white; text-align:left; font-size:30px">Description: Chicago start up company that manages cats</p>
 
     </div>
     <br><br>
@@ -26,7 +26,7 @@
   <p style="color:white; text-align:left; font-size:30px">Duration : April 1st to May 1st</p>
     <p style="color:white; text-align:left; font-size:30px">payment: $698</p>
     <p style="color:white; text-align:left; font-size:30px">Contact Infromation: (111) 863-5739</p>
-      <p style="color:white; text-align:left; font-size:30px">Description: ????<br>?????<br>????</p>
+      <p style="color:white; text-align:left; font-size:30px">Description: TBD </p>
 </div>
 <br><br>
 
